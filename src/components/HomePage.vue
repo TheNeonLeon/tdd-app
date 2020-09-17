c353dcf53a129f122119aee04d2193c623d9f701
Jensengbg-Leon-Listo <leon.listo@hotmail.com>
2020-09-17 16:04:26 +0200

    db, homepage, router update

---
+++ src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <header>
+          <h1>The meetup hub</h1>
+      </header>
+      <ul>
+          <li>
+
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: () => {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
